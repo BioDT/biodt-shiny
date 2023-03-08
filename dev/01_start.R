29acea26178097a10fb5695f48da3fe078978fbe
Tomas Martinovic <tomas.martinovic@vsb.cz>
2023-03-08 15:25:54 +0100

    first change to start.R

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,8 +20,8 @@
 ##
 golem::fill_desc(
   pkg_name = "BioDTShiny", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
+  pkg_title = "BioDT Shiny App", # The Title of the package containing the App
+  pkg_description = "This app is for the interaction with BioDT.", # The Description of the package containing the App
   author_first_name = "AUTHOR_FIRST", # Your First Name
   author_last_name = "AUTHOR_LAST", # Your Last Name
   author_email = "AUTHOR@MAIL.COM", # Your Email
@@ -33,11 +33,11 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_mit_license("BioDT") # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
+usethis::use_code_of_conduct(contact = "")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
