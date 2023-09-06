b84e0ba1504c276579f5c22cc03cf756fd2079f4
Tomas Martinovic <tomas.martinovic@vsb.cz>
2023-09-06 17:26:17 +0200

    added loading maps and lookup tables from lexis into pollinators use case

--- R/mod_BEEHAVE.R
+++ R/mod_BEEHAVE.R
@@ -9,6 +9,9 @@
 #' @importFrom shiny NS tagList
 #' @import gridlayout
 #' @importFrom DT DTOutput
+#' @importFrom leaflet leafletOutput
+#' @importFrom shinycssloaders withSpinner
+#' @importFrom shinyjs hidden
 mod_beehave_ui <- function(id) {
   ns <- NS(id)
   tagList(
@@ -34,24 +37,47 @@ mod_beehave_ui <- function(id) {
         full_screen = TRUE,
         card_title("Input Map"),
         card_body(
-          shiny::selectInput(ns("input_map_list"),
+          shiny::selectInput(ns("map_list"),
                              label = "Choose input map",
                              choices = NULL),
-          plotOutput(ns("input_map_plot")))
+          shinyjs::hidden(shiny::actionButton(ns("load_maps"),
+                                              label = "Load maps")),
+          shinycssloaders::withSpinner(leaflet::leafletOutput(ns("input_map_plot")))
+        )
       ),
       grid_card(
         area = "locations",
         full_screen = TRUE,
         min_height = "500px",
         card_title("Locations"),
-        card_body()
+        card_body(
+          shiny::numericInput(ns("latitude"),
+                              label = "Latitude",
+                              value = 0,
+                              min = -90,
+                              max = 90),
+          shiny::numericInput(ns("longitude"),
+                              label = "longitude",
+                              value = 0,
+                              min = -180,
+                              max = 180),
+          shiny::fileInput(ns("upload_dbf"),
+                           label = "Upload dbf locations file",
+                           accept = ".dbf")
+        )
       ),
       grid_card(
         area = "lookup_table",
         full_screen = TRUE,
         min_height = "500px",
         card_title("Lookup Table"),
-        card_body(DTOutput(ns("lookup_table")))
+        card_body(
+          shiny::selectInput(ns("lookup_list"),
+                             label = "Choose input lookup table",
+                             choices = NULL),
+          shinyjs::hidden(shiny::actionButton(ns("load_lookup"),
+                                              label = "Load lookup table")),
+          DTOutput(ns("lookup_table")))
       ),
       grid_card(
         area = "parameters_table",
@@ -97,58 +123,248 @@ mod_beehave_ui <- function(id) {
 #'
 #' @importFrom DT renderDT
 #' @importFrom shinipsum random_DT random_ggplot
+#' @importFrom shinyjs showElement hideElement
+#' @importFrom r4lexis download_file_from_dataset extract_dataset get_dataset_file_list
+#' @importFrom terra rast plet
+#' @importFrom purrr map_chr pluck
+#' @importFrom leaflet renderLeaflet
+#' @importFrom golem print_dev
 #'
 #' @noRd
 mod_beehave_server <- function(id, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
+    # Define reactive variables ----
     r_beehave <- reactiveValues(
       input_map_list = NULL,
-      map_files = NULL
+      input_rast_map = NULL,
+      input_leaflet_map = NULL,
+      map_ds = NULL,
+      map_files = NULL,
+      maps_loaded = FALSE,
+      map_output = NULL,
+      lookup_ds = NULL,
+      lookup_files = NULL,
+      lookup_loaded = FALSE,
+      lookup_table = NULL,
+      input_lookup_list = NULL,
     )
     
+    # Define beehave variables ----
+    temp_dir <- file.path(tempdir(), "beehave")
+    if (!dir.exists(temp_dir)) {
+      dir.create(temp_dir)
+    }
+    beehave_maps_dir <- file.path(temp_dir, "beehave_maps")
+    if (!dir.exists(beehave_maps_dir)) {
+      dir.create(beehave_maps_dir)
+    }
+    beehave_lookup_dir <- file.path(temp_dir, "beehave_lookup")
+    if (!dir.exists(beehave_lookup_dir)) {
+      dir.create(beehave_lookup_dir)
+    }
+    beehave_locations_dir <-
+      file.path(temp_dir, "beehave_locations")
+    if (!dir.exists(beehave_locations_dir)) {
+      dir.create(beehave_locations_dir)
+    }
+    beehave_parameter_dir <-
+      file.path(temp_dir, "beehave_parameter")
+    if (!dir.exists(beehave_parameter_dir)) {
+      dir.create(beehave_parameter_dir)
+    }
+    
+    # Load maps and lookup table list ----
     observeEvent(r$lexis_token,
                  {
                    req(r$lexis_token,
                        r$lexis_dataset_list)
                    
-                   ds_id <- purrr::map_chr(r$lexis_dataset_list, function(x){purrr::pluck(x, "location", "internalID")})
-                   ds_titles <- purrr::map(r$lexis_dataset_list, function(x){purrr::pluck(x, "metadata", "title")}) |> unlist()
                    
-                   ind_titles_true <- purrr::map_lgl(ds_titles, ~length(.x)>0)
-                   ind_maps <- ds_titles == "Beehave Input Maps"
+                   r_beehave$map_ds <-
+                     r4lexis::extract_dataset(r$lexis_dataset_list,
+                                              "Beehave Input Maps",
+                                              "metadata",
+                                              "title")
                    
-                   r_beehave$map_files <- r4lexis::get_dataset_file_list(
-                     r$lexis_token,
-                     internalID = ds_id[ind_titles_true][ind_maps],
-                     project = "biodt_development"
-                   )
+                   r_beehave$map_files <-
+                     r4lexis::get_dataset_file_list(
+                       r$lexis_token,
+                       internalID = r_beehave$map_ds$location$internalID,
+                       project = "biodt_development"
+                     )
                    
-                   beehave_map_list <- r_beehave$map_files$contents |>
-                     purrr::map_chr(purrr::pluck("name")) 
+                   beehave_map_list <-
+                     r_beehave$map_files$contents |>
+                     purrr::map_chr(purrr::pluck("name"))
                    
-                   r_beehave$input_map_list <-  beehave_map_list[stringr::str_detect(beehave_map_list,
-                                                                             ".tif$")]
-                   
+                   r_beehave$input_map_list <-
+                     beehave_map_list[stringr::str_detect(beehave_map_list,
+                                                          ".tif$")]
                    
                    updateSelectInput(
                      session = session,
-                     inputId = "input_map_list",
-                     selected = beehave_map_list[1],
-                     choices = beehave_map_list
+                     inputId = "map_list",
+                     selected = r_beehave$input_map_list[1],
+                     choices = r_beehave$input_map_list
                    )
                    
+                   golem::print_dev(input$map_list)
                    
-                   ind_lookup_table <- ds_titles == "Beehave Input Lookup"
+                   # Show download maps button if they are not downloaded
+                   if (length(r_beehave$input_map_list) > 0 &
+                       r_beehave$maps_loaded == FALSE) {
+                     golem::print_dev("Showing map loading button.")
+                     shinyjs::showElement("load_maps")
+                   }
+                   
+                   r_beehave$lookup_ds <-
+                     r4lexis::extract_dataset(r$lexis_dataset_list,
+                                              "Beehave Input Lookup",
+                                              "metadata",
+                                              "title")
+                   
+                   r_beehave$lookup_files <-
+                     r4lexis::get_dataset_file_list(
+                       r$lexis_token,
+                       internalID = r_beehave$lookup_ds$location$internalID,
+                       project = "biodt_development"
+                     )
+                   
+                   beehave_lookup_list <-
+                     r_beehave$lookup_files$contents |>
+                     purrr::map_chr(purrr::pluck("name"))
+                   
+                   r_beehave$input_lookup_list <-
+                     beehave_lookup_list[stringr::str_detect(beehave_lookup_list,
+                                                          ".csv$")]
+                   
+                   updateSelectInput(
+                     session = session,
+                     inputId = "lookup_list",
+                     selected = r_beehave$input_lookup_list[1],
+                     choices = r_beehave$input_lookup_list
+                   )
+                   
+                   # Show download maps button if they are not downloaded
+                   if (length(r_beehave$input_lookup_list) > 0 &
+                       r_beehave$lookup_loaded == FALSE) {
+                     golem::print_dev("Showing lookup loading button.")
+                     shinyjs::showElement("load_lookup")
+                   }
+                   
                  })
     
+    # Download maps from Lexis DDI ----
     
-    output$input_map_plot <- renderPlot({
-      random_ggplot(type = "hex")
+    observeEvent(input$load_maps,
+                 
+                 {
+                   req(r$lexis_token)
+                   
+                   non_dev <- TRUE
+                   if (length(list.files(beehave_maps_dir)) > 0 &
+                       golem::app_dev()) {
+                     r_beehave$maps_loaded <- TRUE
+                     shinyjs::hideElement("load_maps")
+                     non_dev <- NULL
+                   }
+                   
+                   req(non_dev,
+                       cancelOutput = TRUE)
+                   golem::print_dev("Input maps button pressed.")
+                   r4lexis::download_file_from_dataset(
+                     r$lexis_token,
+                     r_beehave$map_ds,
+                     destination_path = beehave_maps_dir,
+                     extract = TRUE
+                   )
+                   
+                   if (length(list.files(beehave_maps_dir)) > 0) {
+                     golem::print_dev("Input maps downloaded.")
+                     r_beehave$maps_loaded <- TRUE
+                     shinyjs::hideElement("load_maps")
+                   } else {
+                     golem::print_dev("Input maps NOT downloaded.")
+                   }
+                   
+                 })
+    
+    # Helper function to observe multiple events
+    listen_maps_input <- reactive({
+      list(input$map_list,
+           r_beehave$maps_loaded)
     })
+    observeEvent(listen_maps_input(),
+                 {
+                   req(r_beehave$maps_loaded,
+                       input$map_list)
+                   golem::print_dev("Loading input map raster and making leaflet visualization.")
+                   r_beehave$input_rast_map <-
+                     terra::rast(file.path(beehave_maps_dir, input$map_list))
+                   r_beehave$input_leaflet_map <-
+                     terra::plet(r_beehave$input_rast_map,
+                                 tiles = "Streets",
+                                 alpha = 0.4)
+                 })
+    
+    # Download lookup tables from Lexis DDI ----
+    
+    observeEvent(input$load_lookup,
+                 
+                 {
+                   req(r$lexis_token)
+                   
+                   non_dev <- TRUE
+                   if (length(list.files(beehave_lookup_dir)) > 0 &
+                       golem::app_dev()) {
+                     r_beehave$lookup_loaded <- TRUE
+                     shinyjs::hideElement("load_lookup")
+                     non_dev <- NULL
+                   }
+                   
+                   req(non_dev,
+                       cancelOutput = TRUE)
+                   golem::print_dev("Input lookup button pressed.")
+                   r4lexis::download_file_from_dataset(
+                     r$lexis_token,
+                     r_beehave$lookup_ds,
+                     destination_path = beehave_lookup_dir,
+                     extract = TRUE
+                   )
+                   
+                   if (length(list.files(beehave_lookup_dir)) > 0) {
+                     golem::print_dev("Input lookup tables downloaded.")
+                     r_beehave$lookup_loaded <- TRUE
+                     shinyjs::hideElement("load_lookup")
+                   } else {
+                     golem::print_dev("Input lookup tables NOT downloaded.")
+                   }
+                   
+                 })
+    
+    # Helper function to observe multiple events
+    listen_lookup_input <- reactive({
+      list(input$lookup_list,
+           r_beehave$lookup_loaded)
+    })
+    observeEvent(listen_lookup_input(),
+                 {
+                   req(r_beehave$lookup_loaded,
+                       input$lookup_list)
+                   golem::print_dev("Loading lookup tables.")
+                   r_beehave$lookup_table <-
+                     readr::read_csv(file.path(beehave_lookup_dir, input$lookup_list),
+                                     show_col_types = FALSE)
+                 })
+    
+    # Render outputs ----
+    
+    output$input_map_plot <-
+      leaflet::renderLeaflet(r_beehave$input_leaflet_map)
     output$lookup_table <- DT::renderDT({
-      random_DT(50, 4)
+      DT::datatable(r_beehave$lookup_table)
     })
     output$parameters_table <- DT::renderDT({
       random_DT(50, 3)
