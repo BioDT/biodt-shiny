e6e9c5320b94725d6383cafd9875da55e0f63dba
Tomas Martinovic <tomas.martinovic@vsb.cz>
2024-01-16 09:49:06 +0100

    update CWR

--- R/mod_cwr.R
+++ R/mod_cwr.R
@@ -17,18 +17,18 @@ mod_cwr_ui <- function(id) {
       choices = list("Lathyrus Sativus" = "lathyrus_sativus")
     ),
     shiny::sliderInput(
-      ns("acidity"),
-      label = "Select acidity range",
-      min = 0,
-      max = 1,
-      value = c(0, 1)
-    ),
-    shiny::sliderInput(
-      ns("occurrence"),
-      label = "Select occurence",
+      ns("suitability"),
+      label = "Select suitability range",
       min = 0,
       max = 500,
       value = c(0, 500)
+    ),
+    shiny::sliderInput(
+      ns("predicted_presence"),
+      label = "Select predicted presence/absence range",
+      min = 0,
+      max = 1,
+      value = c(0, 1)
     ),
     shiny::actionButton(
       ns("recompute"),
@@ -49,44 +49,65 @@ mod_cwr_server <- function(id,
   shiny::moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
+    # Initialize reactiveValues for CWR pDT ----
+    r_cwr <- shiny::reactiveValues(map = leaflet::leaflet() |> leaflet::addTiles(),
+                                   modelled_ras = NULL)
+    
     # For now there is a CWR variable available which contains and example data for Lathyrus Sativus
     
-    CWR <- terra::rast(biodtshiny_example("Lathyrus_sativus-Outputs.nc"),
-                       drivers = "NETCDF") |>
-      leaflet::projectRasterForLeaflet(method = "bilinear")
+
+    # Load data when CWR page opens ----
+    observeEvent(r$page_name,
+                 {
+                   if (r$page_name == "Crop wild relatives and genetic resources for food security") {
+                     golem::print_dev("Loading data for CWR")
+                     load(biodtshiny_example("CWR_demo.RData"),
+                          .GlobalEnv)
+                     r_cwr$modelled_ras <- terra::rast(biodtshiny_example("Lathyrus_sativus-Outputs.nc"),
+                                        drivers = "NETCDF") |>
+                       leaflet::projectRasterForLeaflet(method = "bilinear")
+                   }
+                 })
     
-    
-    r_cwr <- shiny::reactiveValues(map = leaflet::leaflet() |> leaflet::addTiles(),
-                                   data = CWR)
+
     shiny::observeEvent(input$recompute,
                         ignoreInit = TRUE,
                         {
                           shiny::req(
                             input$species,
-                            input$acidity,
-                            input$occurrence,
+                            input$predicted_presence,
+                            input$suitability,
+                            r_cwr$modelled_ras,
                             r$page_name == "Crop wild relatives and genetic resources for food security"
                           )
                           if (input$species == "lathyrus_sativus") {
+                            layer_names <- c("Suitability",
+                                             "Predicted Presence/Absence")
+                            
+                            modelled_ras_sub <- r_cwr$modelled_ras
                             r_cwr$map <- leaflet::leaflet() |>
                               leaflet::addTiles()
                             # Specific part for the current example ----
-                            r_cwr$data[[1]] <- CWR |>
+                            modelled_ras_sub[[1]] <- modelled_ras_sub |>
                               terra::subset(1) |>
-                              terra::clamp(lower = input$occurrence[1],
-                                           upper = input$occurrence[2])
-                            r_cwr$data[[2]] <- CWR |>
+                              terra::clamp(lower = input$suitability[1],
+                                           upper = input$suitability[2],
+                                           values = FALSE)
+                            modelled_ras_sub[[2]] <- modelled_ras_sub |>
                               terra::subset(2) |>
-                              terra::clamp(lower = input$acidity[1],
-                                           upper = input$acidity[2])
+                              terra::clamp(lower = input$predicted_presence[1],
+                                           upper = input$predicted_presence[2],
+                                           values = FALSE)
                             
                             # This could be probably generic ----
-                            n_layers <- dim(r_cwr$data)[3]
+                            n_layers <- dim(modelled_ras_sub)[3]
                             for (i in seq_len(n_layers)) {
                               golem::cat_dev("\nLayer ", i, " is being computed.\n")
-                              layer <- r_cwr$data |> terra::subset(i)
+                              layer <- modelled_ras_sub |> terra::subset(i)
                               layer_range <-
-                                layer |> terra::minmax() |> range() |> round()
+                                r_cwr$modelled_ras |> terra::minmax() |> range()
+                              layer_range[1] <- floor(layer_range[1])
+                              layer_range[2] <- ceiling(layer_range[2])
                               layer_bins <-
                                 layer |> as.vector() |> unique() |> length()
                               
@@ -111,17 +132,24 @@ mod_cwr_server <- function(id,
                                 leaflet::addRasterImage(
                                   layer,
                                   project = FALSE,
-                                  group = i |> as.character(),
+                                  group = layer_names[i],
                                   opacity = max((1 - (i /
                                                         5)), 0.2)
                                 ) |>
                                 leaflet::addLegend(pal = pal,
                                                    values = layer_range,
-                                                   group = i |> as.character())
+                                                   group = layer_names[i],
+                                                   labels = layer_names[i])
                             }
+                            if (exists("absences_sf")) {
+                              r_cwr$map <- r_cwr$map 
+                                
+                            }
+                            
+                            
                             r_cwr$map <- r_cwr$map |>
                               leaflet::addLayersControl(position = "topleft",
-                                                        overlayGroups = seq_len(n_layers) |> as.character())
+                                                        overlayGroups = layer_names)
                           }
                         })
     
