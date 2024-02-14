1f61b24fddf0b6390f7544445876b9c04b375b16
Simon Rolph <simrol@ceh.ac.uk>
2024-02-14 15:27:15 +0000

    updates to R module

--- R/mod_cultural_ecosystem_services.R
+++ R/mod_cultural_ecosystem_services.R
@@ -4,65 +4,80 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 #' @import leaflet
-mod_cultural_ecosystem_services_ui <- function(id){
+mod_cultural_ecosystem_services_ui <- function(id) {
   ns <- NS(id)
   
   tagList(bslib::page_fluid(
     class = "p-0",
     bslib::navset_tab(
-      
-        bslib::nav_panel("Recreation potential",
-                bslib::card(
-                  title = "rec_pot_map",
-                  full_screen = TRUE,
-                  card_title("Recreation potential mapping"),
-                  card_body(
-                    selectInput("persona",
-                                "Recreation potential persona",
-                                c("
+      bslib::nav_panel(
+        "Recreation potential",
+        bslib::card(
+          title = "rec_pot_map",
+          full_screen = TRUE,
+          card_title("Recreation potential mapping"),
+          card_body(
+            selectInput(
+              "persona",
+              "Recreation potential persona",
+              c("
                                   Hard recreationalist",
-                                  "Soft recreationalist"
-                                  )
-                                ),
-                    leafletOutput(ns("rec_pot_map"), height = 600),
-                    )
-                )
+                "Soft recreationalist")
+            ),
+            leafletOutput(ns("rec_pot_map"), height = 600),
+          )
+        )
       ),
       
-      bslib::nav_panel("Biodiversity",
-                bslib::card(
-                  title = "biodiversity_map",
-                  full_screen = TRUE,
-                  card_title("Biodiversity mapping"),
-                  card_body(leafletOutput(ns("sp_map"), height = 600))
-                )
-      ),
-      
-      bslib::nav_panel("Species distribution models",
-                bslib::card(
-                  title = "sdm_table",
-                  full_screen = TRUE,
-                  card_title("Species distribution models"),
-                  card_body(DT::DTOutput(ns('sp_tbl')))
-                )
+      bslib::nav_panel(
+        "Biodiversity",
+        
+        bslib::card(
+          title = "biodiversity_choice",
+          full_screen = TRUE,
+          card_title("What sort of biodiversity do I want to experience?"),
+          card_body(
+            radioButtons(
+              "radio_group_select",
+              "I'm interested in",
+              c(
+                "  All biodiversity" = "all",
+                '  Cairngorms "big 5"' = "big5",
+                "  Mammals" = "mammals",
+                "  Birds" = "birds",
+                "  Plants" = "plants",
+                "  Amphibians and reptiles" = "herptiles",
+                "  Insects" = "insects"
+              ),
+              inline = T
+            )
+          )
+        ),
+        bslib::card(
+          title = "biodiversity_map",
+          full_screen = TRUE,
+          card_title("Where can I find biodiversity?"),
+          card_body(leafletOutput(ns("sp_map"), height = 600))
+        ),
+        bslib::card(
+          title = "sdm_table",
+          full_screen = TRUE,
+          card_title("Species details"),
+          card_body(DT::DTOutput(ns('sp_tbl')))
+        )
       )
-      
     )
-    )
-  )
+    
+  ))
   
 }
-    
+
 #' cultural_ecosystem_services Server Functions
-#' 
-#' @importFrom DT renderDT
-#' @importFrom shinipsum random_DT random_ggplot
-#' @importFrom shinyjs showElement hideElement
-#' @importFrom r4lexis download_file_from_dataset extract_dataset get_dataset_file_list
+#'
 #' @importFrom terra rast plet
 #' @importFrom purrr map_chr pluck
 #' @importFrom cli hash_md5
@@ -71,103 +86,98 @@ mod_cultural_ecosystem_services_ui <- function(id){
 #' @importFrom golem print_dev
 #' @importFrom cicerone Cicerone
 #'
-#' @noRd 
-mod_cultural_ecosystem_services_server <- function(id,r){
-  moduleServer( id, function(input, output, session){
+#' @noRd
+mod_cultural_ecosystem_services_server <- function(id, r) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
     # Define reactive variables ----
-    r_culteco <- reactiveValues(
-      name = "value" 
-    )
+    # r_culteco <- reactiveValues(
+    #   name = "value"
+    # )
     
-    print(r)
-    
+    # wrap the whole thing in a observe page
     observeEvent(r$page_name,
                  {
+                   #RECREATION POTENTIAL MAP
+                   #recreation potential map
+                   output$rec_pot_map <- renderLeaflet({
+                     leaflet() %>%
+                       addTiles() %>%
+                       leaflet::setView(lng = -3.5616,
+                                        lat = 57.0492,
+                                        zoom = 9)
+                   })
                    
-                   print("lexis_token:")
-                   print(r$lexis_token)
+                   #SPECIES MAP
+                   #get species data table
+                   cairngorms_sp_list <-
+                     read.csv("data/uc-ces/biodiversity/cairngorms_sp_list.csv")
                    
-                   print("lexis_dataset_list:")
-                   print(r$lexis_dataset_list)
+                   #species_table
+                   output$sp_tbl = renderDT(
+                     cairngorms_sp_list,
+                     options = list(lengthChange = FALSE),
+                     colnames = c('Key', "Count", "Common name", 'Scientific name'),
+                   )
                    
-                   print("page_name:")
-                   print(r$page_name)
+                   #get raster files
+                   all_sdm_files <-
+                     list.files("data/uc-ces/biodiversity/sdms", full.names = T)
+                   taxon_ids_from_file_names <-
+                     list.files("data/uc-ces/biodiversity/sdms", full.names = F) |>
+                     lapply(
+                       FUN = function(x) {
+                         gsub("prediction_(\\d+)_.*", "\\1", x)
+                       }
+                     ) |>
+                     unlist()
+                   sdm_rasts <- terra::rast(all_sdm_files)
+                   sdm_rasts <-
+                     sdm_rasts[[names(sdm_rasts) == "constrained"]]
+                   names(sdm_rasts) <- taxon_ids_from_file_names
                    
-                   req(r$lexis_token,
-                       r$lexis_dataset_list,
-                       r$page_name == "Ecosystem services")
+                   #calculate biodiversity layer which is shown to the user
+                   mean_biodiversity <-
+                     sdm_rasts |> terra::app(mean)
+                   terra::values(mean_biodiversity)[terra::values(mean_biodiversity) <
+                                                      0.1] <- NA
                    
-                   print("Required information supplied to authenticate with LEXIS")
                    
-                   # Authenticate with Lexis
-                   lexis_token <- r$lexis_token
-                   lexis_token <- "INSERT TOKEN"
                    
-                   # Specify dataset information
-                   dataset_name <- "your_dataset_name"
-                   project_id <- "your_project_id"
+                   #species map
+                   output$sp_map <- renderLeaflet({
+                     leaflet() %>%
+                       addTiles() %>%
+                       addRasterImage(
+                         mean_biodiversity,
+                         group = "Biodiversity Model",
+                         opacity = 0.4,
+                         colors = "viridis"
+                       ) %>%
+                       addTiles(
+                         urlTemplate = "https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?style=orange.marker&bin=hex",
+                         attribution = "GBIF",
+                         group = "Biodiversity Data"
+                       ) %>%
+                       leaflet::setView(lng = -3.5616,
+                                        lat = 57.0492,
+                                        zoom = 9) %>%
+                       addLayersControl(
+                         baseGroups = c("Open Street Map"),
+                         overlayGroups = c("Biodiversity Model", "Biodiversity Data"),
+                         options = layersControlOptions(collapsed = FALSE)
+                       ) %>%
+                       hideGroup("Biodiversity Data")
+                   })
                    
-                   # Retrieve dataset list
-                   dataset_list <- r$lexis_dataset_list
-                   
-                   # Find dataset by name
-                   dataset <- dataset_list[[dataset_name]]
-                   
-                   # Retrieve dataset files
-                   dataset_files <- get_dataset_file_list(lexis_token, dataset$location$internalID, project_id)
-                   
-                   # Download dataset files to local directory
-                   destination_path <- tempdir()
-                   data <- download_file_from_dataset(lexis_token, dataset, destination_path, extract = TRUE)
-                   print(data)
-                   
-                 }
-    )
-    
-    
-    
-    #recreation potential map
-    output$rec_pot_map <- renderLeaflet({
-      leaflet() %>% 
-        addTiles() %>%
-        leaflet::setView( lng = -3.5616, lat = 57.0492, zoom = 9 )
-    })  
-    
-    #species map
-    output$sp_map <- renderLeaflet({
-      leaflet() %>% 
-        addTiles() %>%
-        addTiles(urlTemplate="https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?style=orange.marker&bin=hex",
-                 attribution ="GBIF") %>%
-        leaflet::setView( lng = -3.5616, lat = 57.0492, zoom = 9 )
-    }) 
-    
-    #species list table
-    
-    # Get data from LEXIS
-    
-    
-    
-    sp_list_table <- data.frame(species = c("Bufo bufo","Froggus froggus") ,
-                                common_name = c("Toad","Frog"),
-                                last_run= c("2023-04-23","2023-04-25"),
-                                n_records = c(2042,4320),
-                                view_report=rep("View report",2),
-                                rerun_model = rep("Rerun model",2))
-    
-    output$sp_tbl = renderDT(
-      sp_list_table, 
-      options = list(lengthChange = FALSE),
-      colnames = c('Species',"Common name", "Last run", 'Number of records', '',   ''), 
-    )
- 
+
+                 })
   })
 }
-    
+
 ## To be copied in the UI
 # mod_cultural_ecosystem_services_ui("cultural_ecosystem_services_1")
-    
+
 ## To be copied in the server
 # mod_cultural_ecosystem_services_server("cultural_ecosystem_services_1")
