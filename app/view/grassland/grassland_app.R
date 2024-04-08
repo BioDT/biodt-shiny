42ed1dbb142d37e0bb7a90b99f868b7066009443
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-04-08 13:40:25 +0200

    quick fix

--- app/view/grassland/grassland_app.R
+++ app/view/grassland/grassland_app.R
@@ -15,15 +15,17 @@ box::use(
 grassland_app_ui <- function(id, theme) {
   ns <- shiny::NS(id)
 
-  grassland_control_ui(ns("grassland_control")),
-  layout_column_wrap(
-    width = NULL,
-    fill = FALSE,
-    style = htmltools::css(grid_template_columns = "3fr 1fr"),
-    mod_grassland_inputmap_ui(ns("grassland_inputmap")),
-    mod_grassland_location_ui(ns("grassland_location")),
+  tagList(
+    grassland_control_ui(ns("grassland_control")),
+    layout_column_wrap(
+      width = NULL,
+      fill = FALSE,
+      style = htmltools::css(grid_template_columns = "3fr 1fr"),
+      mod_grassland_inputmap_ui(ns("grassland_inputmap")),
+      mod_grassland_location_ui(ns("grassland_location")),
+    ),
+    mod_grassland_outputplot_ui(ns("grassland_outputplot"))
   )
-  mod_grassland_outputplot_ui(ns("grassland_outputplot"))
 }
 
 #' @export
