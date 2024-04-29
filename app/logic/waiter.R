63f45c865cb8fe46870a5b61f72dbdb4f29ed13c
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-04-29 11:48:49 +0200

    styles of waiter move directly to it

--- app/logic/waiter.R
+++ app/logic/waiter.R
@@ -1,5 +1,5 @@
 box::use(
-  shiny[tagList, h3],
+  shiny[tagList, h3, tags],
   waiter[spin_loaders],
 )
 
@@ -10,8 +10,19 @@ waiter_text <- function(
       id = 19,
       color = "#414f2f"
     )) {
+  css <- tags$style(
+    ".waiter-overlay-content{
+      background-image: url('./img/forest2.png');
+      background-repeat: no-repeat;
+      background-size: 100%;
+      background-position: bottom;
+      display: flex;
+    }"
+  )
+
   tagList(
     spinner,
-    message
+    message,
+    css
   )
 }
