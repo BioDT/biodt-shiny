1ed5d5584f6332e786f0d4fcc8f87168217e7a9a
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-05-02 09:08:41 +0200

    two new args for waiter_text fn + linter run

--- app/logic/waiter.R
+++ app/logic/waiter.R
@@ -6,19 +6,28 @@ box::use(
 #' @export
 waiter_text <- function(
     message = h3("Working..."),
+    style = "color: #414f2f;",
+    css,
     spinner = spin_loaders(
       id = 19,
       color = "#414f2f"
     )) {
-  css <- tags$style(
-    ".waiter-overlay-content{
-      background-image: url('./img/forest2.png');
-      background-repeat: no-repeat;
-      background-size: 100%;
-      background-position: bottom;
-      display: flex;
-    }"
-  )
+  if (missing(css)) { # default
+    css <- tags$style(
+      paste0(
+        ".waiter-overlay-content {
+          background-image: url('./img/forest2.png');
+          background-repeat: no-repeat;
+          background-size: 100%;
+          background-position: bottom;
+          display: flex; ",
+        style,
+        "}"
+      )
+    )
+  } else { # if someone wants to completely rewrite css of the loader
+    css <- css
+  }
 
   tagList(
     css,
