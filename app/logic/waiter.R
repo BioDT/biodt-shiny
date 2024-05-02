c965a2ef6fbb60cfd7f14b1434c6e9166565737b
Ondrej Salamon <ondrej.salamon@vsb.cz>
2024-05-02 15:27:36 +0200

    Waiter styles move from scss (#36)
    * styles of waiter move directly to it
    
    * minor edit of previous, style executed before content
    
    * two new args for waiter_text fn + linter run

--- app/logic/waiter.R
+++ app/logic/waiter.R
@@ -1,17 +1,37 @@
 box::use(
-  shiny[tagList, h3],
+  shiny[tagList, h3, tags],
   waiter[spin_loaders],
 )
 
 #' @export
 waiter_text <- function(
     message = h3("Working..."),
+    style = "color: #414f2f;",
+    css,
     spinner = spin_loaders(
       id = 19,
       color = "#414f2f"
     )) {
+  if (missing(css)) { # default
+    css <- tags$style(
+      paste0(
+        ".waiter-overlay-content {
+          background-image: url('./img/forest2.png');
+          background-repeat: no-repeat;
+          background-size: 100%;
+          background-position: bottom;
+          display: flex; ",
+        style,
+        "}"
+      )
+    )
+  } else { # if someone wants to completely rewrite css of the loader
+    css <- css
+  }
+
   tagList(
+    css,
     spinner,
-    message
+    message,
   )
 }
