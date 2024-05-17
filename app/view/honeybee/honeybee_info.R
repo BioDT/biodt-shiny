61d9560bac2a2b2b31a3d7bbe0d132be7f3695f4
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-05-17 14:59:53 +0200

    fix typos

--- app/view/honeybee/honeybee_info.R
+++ app/view/honeybee/honeybee_info.R
@@ -18,15 +18,15 @@ honeybee_info_ui <- function(id) {
         ),
         tags$p(
           class = "pt-3",
-          "The HONEYBEE prototype Digtial Twin (pDT) is based on the mechanistic simulation model BEEHAVE (Becher et al. 2014,", tags$a("https://doi.org/10.1111/1365-2664.12222", href = "https://doi.org/10.1111/1365-2664.12222"), ")."
+          "The HONEYBEE prototype Digital Twin (pDT) is based on the mechanistic simulation model BEEHAVE (Becher et al. 2014,", tags$a("https://doi.org/10.1111/1365-2664.12222", href = "https://doi.org/10.1111/1365-2664.12222"), ")."
         ),
         tags$p("Model descriptions of BEEHAVE and additional information can be found on", tags$a("https://beehave-model.net/", href = "https://beehave-model.net/"), "."),
-        tags$p("The pollinator prototype Digital Twin needs input on floral resources. As a demonstration example we use a land cover map provided by Preidl et al. (2020,", tags$a("https://doi.org/10.1594/PANGAEA.910837", href = "https://doi.org/10.1594/PANGAEA.910837"), ")."),
+        tags$p("The Honeybee prototype Digital Twin needs input on floral resources. As a demonstration example we use a land cover map provided by Preidl et al. (2020,", tags$a("https://doi.org/10.1594/PANGAEA.910837", href = "https://doi.org/10.1594/PANGAEA.910837"), ")."),
         tags$p("Weather data are requested from the Deutscher Wetterdienst (DWD) using an API provided by the R package rdwd (", tags$a("https://cran.r-project.org/web/packages/rdwd/index.html", href = "https://cran.r-project.org/web/packages/rdwd/index.html"), ")."),
         tags$p("Source code and scripts of the pDT can be found at ", tags$a(icon("github"), "https://github.com/BioDT", href = "https://github.com/BioDT"), "."),
         tags$h4("Contributors"),
-        tags$p("Tomas Martinovic and Ondrej Salamon, IT4Innovations, VSB – Technical University of Ostrava, 17. listopadu 2172/15, 708 00 Ostrava-Poruba, Czech Republic"),
         tags$p("Jürgen Groeneveld and Volker Grimm, Department of Ecological Modelling ,Helmholtz Centre for Environmental Research - UFZ, Permoserstr. 15, 7 04318 Leipzig, Germany"),
+        tags$p("Tomas Martinovic and Ondrej Salamon, IT4Innovations, VSB – Technical University of Ostrava, 17. listopadu 2172/15, 708 00 Ostrava-Poruba, Czech Republic"),        
         tags$p("Tuomas Rossi and Kata Sara-aho, CSC – IT Center for Science Ltd., P.O. Box 405, 02101 Espoo, Finland.")
       )
     ),
