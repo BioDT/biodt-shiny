3540c3d95dfdd5f7e1c8de3288bfa1cce8483469
Kata Sara-aho <kata.sara-aho@csc.fi>
2024-07-02 14:55:57 +0300

    disease outbreak introduction text update

--- app/view/disease_outbreaks/info/disease_info.R
+++ app/view/disease_outbreaks/info/disease_info.R
@@ -15,12 +15,15 @@ disease_info_ui <- function(id, i18n) {
           class = "greeting display-4 font-weight-bold",
           i18n$translate("Disease Outbreaks")
         ),
-        tags$h6(i18n$translate("Subheading - Disease Outbreaks")),
         tags$p(
           class = "pt-3",
-          i18n$translate("[EN] Lorem ipsum...")
-        )
-      ),
+          "The BioDT wild boar (Sus scrofa)-African swine fever (ASF) prototype digital twin (pDT)implements a stochastic, spatially- and temporally-explicit, and individually-baselandscape-level mechanistic model as a digital twin aimed at providing informed support for management decisions in response to the spread of African swine fever in European wild boar populations."
+        ),
+        tags$p("The model incorporates wild boar ecology and ASF epidemiology to simulate infection dynamics in the wild boar population. The first run of a wild boar-ASF model simulation is triggered when habitat structure data are supplied to the model. Each time additional information (wild boar locality data, ASF infection detections) is provided, the model reruns, incorporating the new data and returning an updated, dynamic prediction of African swine fever infection including identification of active and recovered cases as well as incidents of wild boar death due to the virus."),
+        tags$p("Read more about the pDT here:", tags$a("https://riojournal.com/article/125521/", href = "https://riojournal.com/article/125521/"), "."),
+        tags$p("Read more about the wild boar-ASF simulation model here:", tags$a("https://doi.org/10.1007/s00477-016-1358-8", href = "https://doi.org/10.1007/s00477-016-1358-8"), "."),
+        tags$p("Video source: Alexis Lours, Sus scrofa Linnaeus, 1758 observed in France,", tags$a("https://www.inaturalist.org/observations/197802817", href = "https://www.inaturalist.org/observations/197802817"), "."),
+       ),
     ),
     column(
       width = 6,
@@ -31,8 +34,6 @@ disease_info_ui <- function(id, i18n) {
           src = "static/img/Alexis-Lours-Sus-scrofa-Linnaeus.gif",
           style = "width: 50vw; height: 100vh; max-height: 1000px; object-fit: cover;"
         ),
-        tags$p(
-          "Credits: fabd06, Sus scrofa Linnaeus, 1758 observed in France, https://www.inaturalist.org/observations/197457782")
       )
     )
   )
