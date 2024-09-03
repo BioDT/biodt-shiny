29e12d2a3a97310d762d7434d9e79b65328361e7
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-09-03 11:40:15 +0200

    minor edit

--- app/view/disease_outbreaks/disease_outbreaks_main.R
+++ app/view/disease_outbreaks/disease_outbreaks_main.R
@@ -6,7 +6,7 @@ box::use(
 box::use(
   app/view/disease_outbreaks/info/disease_info[disease_info_ui, disease_info_server],
   app/view/disease_outbreaks/disease_app/disease_app[disease_app_ui, disease_app_server],
-  app/view/disease_outbreaks/disease_outbreaks_contributors[disease_outbreaks_contributors_ui, disease_outbreaks_contributors_server], # nolint
+  app/view/disease_outbreaks/disease_outbreaks_contributors[disease_outbreaks_contributors_ui],
 )
 
 #' @export
