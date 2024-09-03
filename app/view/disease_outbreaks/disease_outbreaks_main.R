79ff3788c5ce86959e118bdae0240846c3010b19
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-09-03 11:30:03 +0200

    add "Contributors" page

--- app/view/disease_outbreaks/disease_outbreaks_main.R
+++ app/view/disease_outbreaks/disease_outbreaks_main.R
@@ -6,6 +6,7 @@ box::use(
 box::use(
   app/view/disease_outbreaks/info/disease_info[disease_info_ui, disease_info_server],
   app/view/disease_outbreaks/disease_app/disease_app[disease_app_ui, disease_app_server],
+  app/view/disease_outbreaks/disease_outbreaks_contributors[disease_outbreaks_contributors_ui, disease_outbreaks_contributors_server], # nolint
 )
 
 #' @export
@@ -26,6 +27,15 @@ disease_outbreaks_main_ui <- function(id, i18n) {
       icon = icon("bugs"),
       disease_app_ui(
         ns("disease_app"), i18n
+      )
+    ),
+    nav_panel(
+      title = i18n$translate("Contributors"),
+      value = "Contributors",
+      icon = icon("sitemap"),
+      disease_outbreaks_contributors_ui(
+        ns("disease_outbreaks_contributors"),
+        i18n
       )
     )
   )
