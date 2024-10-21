0f104a801925145aa384b25e4aedbaf1f97729f6
Tomas Martinovic <tomas.martinovic@vsb.cz>
2024-10-21 15:16:32 +0200

    remove  open street map from groups and move input to ovelaygroups

--- app/logic/disease_outbreaks/disease_leaflet_map.R
+++ app/logic/disease_outbreaks/disease_leaflet_map.R
@@ -1,7 +1,6 @@
 box::use(
   terra[rast, project],
-  leaflet[tileOptions, leaflet, addTiles, addRasterImage, setView, leafletProxy, removeImage, addRasterLegend, addLegend],
-  leaflet.extras[addGroupedLayersControl, groupedLayersControlOptions]
+  leaflet[tileOptions, leaflet, addTiles, addRasterImage, setView, leafletProxy, removeImage, addRasterLegend, addLegend, addLayersControl],
 )
 
 #' @export
@@ -11,9 +10,9 @@ read_and_project_raster <- function(map_full_path) {
 
 #' @export
 disease_leaflet_map <- function(map_raster,
-                                add_control = TRUE, 
+                                add_control = TRUE,
                                 main_map_features = TRUE) {
-  
+
   leaflet_map <- leaflet() |>
     addTiles(group = "Open Street Map") |>
     setView(
@@ -26,15 +25,10 @@ disease_leaflet_map <- function(map_raster,
       opacity = 0.9,
       project = FALSE,
       options = tileOptions(zIndex = 1000),
-      group = "Disease layer"
+      group = "Input layer"
     ) |>
-    addGroupedLayersControl(
-      baseGroups = c("Open Street Map", "Disease layer"),
-      options = groupedLayersControlOptions(
-            collapsed = FALSE,
-            exclusiveGroups = "Open Street Map",
-            groupsCollapsable = FALSE
-          )
+    addLayersControl(
+      overlayGroups = c("Input layer")
     )
 
     return(leaflet_map)
