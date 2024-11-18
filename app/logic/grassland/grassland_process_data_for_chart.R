fa6856c78b05fc1b63f03ea006f05e33544afbc4
Ondřej Salamon <ondrej.salamon@vsb.cz>
2024-11-18 13:34:17 +0100

    add list all files in grassland's data directory

---
+++ app/logic/grassland/grassland_process_data_for_chart.R
@@ -0,0 +1,4 @@
+dir_with_grasslands_data <- "app/data/grassland"
+dir_project1_example <- paste0("app/data/grassland", "/simulations/project1/output")
+
+get_paths_of_all_files <- list.files(dir_project1_example, full.names = TRUE)
