65644cda19bb915ab721d8b797a6dfc8ac352706
Kata Sara-aho <kata.sara-aho@csc.fi>
2025-05-16 08:49:22 +0300

    grassland ui changes: add button for management datatable, change ui order

--- app/view/grassland/grassland_dynamics/grassland_dynamics_manage_datatable.R
+++ app/view/grassland/grassland_dynamics/grassland_dynamics_manage_datatable.R
@@ -1,5 +1,5 @@
 box::use(
-  shiny[NS, moduleServer, tags, reactiveVal, observeEvent, checkboxInput, uiOutput, renderUI, req],
+  shiny[NS, moduleServer, tags, reactiveVal, observeEvent, checkboxInput, uiOutput, renderUI, req, actionButton, HTML],
   bslib[card, card_header, card_body],
   waiter[Waiter],
   DT[DTOutput, renderDT, datatable],
@@ -20,18 +20,19 @@ grassland_dynamics_manage_datatable_ui <- function(
     id = ns("datatable"),
     class = "mx-md-3 card-shadow mb-2",
     card_header(
-      tags$h2(
-        class = "card_title",
-        i18n$translate("Grassland Management Actions")
+        tags$div(
+          class = "d-flex justify-content-between align-items-center",
+        tags$h2(
+          class = "card_title",
+          i18n$translate("Grassland Management Actions")
+        ),
+        uiOutput(ns("show_management_btn")),
       ),
       # checkboxInput(ns("show_mngmntdata"), "Show Management Data Table", value = FALSE, width = "200px")
     ),
     card_body(
-      tags$div(
-        id = ns("mngmnt_data_table_wrap"),
-        DTOutput(
-          ns("mngmnt_data_table")
-        )
+        uiOutput(
+          ns("mngmnt_data_table_wrap")
       )
     )
   )
@@ -52,43 +53,51 @@ grassland_dynamics_manage_datatable_server <- function(id, data_table, tab_grass
       html = msg,
       color = "rgba(256,256,256,0.9)",
     )
-
-    # show_mngmnttable <- reactiveVal(FALSE)
-    # div_table_wrap_tag <- tags$div(
-    #   id = "div_table_wrap",
-    #   DTOutput(
-    #     ns("mngmnt_data_table")
-    #   )
-    # )
-
-    # observeEvent(input$show_mngmntdata, {
-    #   if (input$show_mngmntdata == TRUE) {
-    #     show_mngmnttable(TRUE)
-    #   } else {
-    #     show_mngmnttable(FALSE)
-    #   }
-    # })
-
-    # observeEvent(show_mngmnttable(), ignoreInit = TRUE, {
-    #   req(data_table)
-    #   if (show_mngmnttable() == FALSE) {
-    #     output$mngmnt_data_table_wrap <- renderUI(NULL)
-    #   }
-    #   if (show_mngmnttable() == TRUE) {
-    #     print(div_table_wrap_tag)
-    #     output$mngmnt_data_table_wrap <- renderUI({
-    #       div_table_wrap_tag
-    #     })
-    #   }
-    # })
-
-    observeEvent(
-      tab_grassland_selected(),
-      ignoreNULL = TRUE,
-      ignoreInit = TRUE,
-      {
-        w$show()
-        output$mngmnt_data_table <- renderDT(
+    
+    # Reactive toggle for showing table
+    show_managementtable <- reactiveVal(FALSE)
+    
+    div_table_wrap_tag <- tags$div(
+      id = "div_table_wrap",
+      DTOutput(ns("mngmnt_data_table"))
+    )
+    
+    # Toggle button UI
+    output$show_management_btn <- renderUI({
+      icon_dir <- if (show_managementtable()) "up" else "down"
+      icon_html <- sprintf('<i class="fa-solid fa-arrow-%s"></i>', icon_dir)
+      actionButton(
+        ns("show_management"),
+        HTML(icon_html),
+        class = "primary-button",
+        `aria-expanded` = tolower(as.character(show_managementtable())),
+        title = if (show_managementtable()) "Collapse management actions table" else "Expand management actions table"
+      )
+    })
+    
+    # Toggle reactive state
+    observeEvent(input$show_management, {
+      show_managementtable(!show_managementtable())
+    })
+    
+    # Show or hide the datatable container
+    observeEvent(show_managementtable(), ignoreInit = TRUE, {
+      req(data_table_reactive())
+      if (show_managementtable()) {
+        output$mngmnt_data_table_wrap <- renderUI({
+          div_table_wrap_tag
+        })
+      } else {
+        output$mngmnt_data_table_wrap <- renderUI(NULL)
+      }
+    })
+    
+    # Render DT on tab change
+    observeEvent(tab_grassland_selected(), ignoreNULL = TRUE, ignoreInit = TRUE, {
+      w$show()
+      req(data_table_reactive())
+      output$mngmnt_data_table <- renderDT(
+        datatable(
           data_table_reactive(),
           editable = list(target = "cell", disable = list(columns = c(0, 1, 2, 3, 4, 5, 6))),
           selection = "none",
@@ -103,8 +112,8 @@ grassland_dynamics_manage_datatable_server <- function(id, data_table, tab_grass
           fillContainer = FALSE,
           rownames = FALSE
         )
-        w$hide()
-      }
-    )
+      )
+      w$hide()
+    })
   })
 }
