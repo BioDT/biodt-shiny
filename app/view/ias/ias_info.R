e01b34965b3baf517e94ae4314ba390c68029b58
Souza Allan <allansou@lx2-870-27966.ad.helsinki.fi>
2025-05-22 09:58:22 +0300

    Updating the landing page info.

--- app/view/ias/ias_info.R
+++ app/view/ias/ias_info.R
@@ -14,11 +14,20 @@ ias_info_ui <- function(id) {
       class = "col-sm-12 col-lg-6",
       style = "height: 100vh;",
       tags$div(
-        class = "col-sm-10 offset-sm-1 text-center mt-5 mb-5",
-        h2("Welcome to the dashboard for the Invasive Alien Species prototype Digital twin."),
-        p("This dashboard provides insights into invasive alien species in Europe and related statistics."),
-        p("Use the navigation tabs to explore detailed visualizations and data summaries."),
-        p(strong("v0.0.5:"), em("last updated on"), em("2025-01-28"))
+        class = "col-sm-10 offset-sm-1 text-left mt-5 mb-5",
+        h2("Overview:"),
+        p("The Prototype Biodiversity Digital Twin for Invasive Alien Species (IAS pDT) provides dynamic projections of the potential distribution of terrestrial naturalised alien plant species across Europe."),
+        p('Leveraging joint species distribution models (jSDMs) and species occurrence data from GBIF and EASIN, IAS pDT estimates individual-species habitat suitability and the level of plant invasion in selected habitats under current and future climate scenarios.'),
+        h2("Key Features:"),
+        tags$ul(
+          tags$li("Regularly updated outputs – Ensures timely and relevant information about invasion risks."),
+          tags$li("High-Risk Area Identification – Detects areas for spatial multispecies prioritisation and proactive intervention."),
+          tags$li("Habitat Suitability Estimation – Enables targeted management and eradication."),
+          tags$li("Climate Scenario Projecting – Assesses invasion risks under different climate conditions and supports long-term strategic management."),
+          tags$li("Prediction Uncertainty Quantification – Enhances decision-making confidence."),
+          tags$li("Maps and data download – Enables external use and knowledge transfer.")
+        ),
+        p(strong("v0.0.9:"), em("last updated on"), em("2025-03-29"))
       )
     ),
     column(
@@ -28,7 +37,7 @@ ias_info_ui <- function(id) {
       tags$div(
         tags$img(
           class = "info-picture",
-          src = "static/img/ias/IAS.png",
+          src = "static/img/ias/IAS2.png",
           alt = "",
         )
       )
