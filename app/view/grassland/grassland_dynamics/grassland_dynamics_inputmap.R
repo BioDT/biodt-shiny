5a1a517dedc77597efe13c27c00950b5a2fb98b8
nithador <tomas.martinovic@vsb.cz>
2025-10-27 17:55:03 +0100

    grassland add marker to the default simulation location

--- app/view/grassland/grassland_dynamics/grassland_dynamics_inputmap.R
+++ app/view/grassland/grassland_dynamics/grassland_dynamics_inputmap.R
@@ -1,7 +1,7 @@
 box::use(
   shiny[NS, moduleServer, tags, observeEvent],
   bslib[card, card_header, card_body],
-  leaflet[setView, leaflet, leafletOptions, leafletOutput, renderLeaflet, addTiles],
+  leaflet[setView, leaflet, leafletOptions, leafletOutput, renderLeaflet, addTiles, addMarkers],
   htmlwidgets[JS],
   waiter[Waiter]
 )
@@ -33,54 +33,53 @@ grassland_dynamics_inputmap_ui <- function(id, i18n) {
 }
 
 #' @export
-grassland_dynamics_inputmap_server <- function(id, coordinates, tab_grassland_selected, i18n) { # nolint: object_length_linter.
+grassland_dynamics_inputmap_server <- function(id, coordinates, tab_grassland_selected, i18n) {
+  # nolint: object_length_linter.
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     # Define waiter ----
-    msg <- waiter_text(message = tags$h3(i18n$t("Loading..."),
-      style = "color: #414f2f;"
-    ))
+    msg <- waiter_text(message = tags$h3(i18n$t("Loading..."), style = "color: #414f2f;"))
     w <- Waiter$new(
       id = ns("inputmap"),
       html = msg,
       color = "rgba(256,256,256,0.9)",
     )
 
-    observeEvent(tab_grassland_selected(),
-      ignoreNULL = TRUE,
-      ignoreInit = TRUE,
-      {
-        w$show()
-        output$leaflet_output <- renderLeaflet({
-          leaflet(
-            options = leafletOptions(
-              zoomControl = TRUE,
-              minZoom = 2,
-            )
+    observeEvent(tab_grassland_selected(), ignoreNULL = TRUE, ignoreInit = TRUE, {
+      w$show()
+      output$leaflet_output <- renderLeaflet({
+        leaflet(
+          options = leafletOptions(
+            zoomControl = TRUE,
+            minZoom = 2,
+          )
+        ) |>
+          addTiles() |>
+          setView(
+            lng = 11.8787,
+            lat = 51.3919,
+            zoom = 9
           ) |>
-            addTiles() |>
-            setView(
-              lng = 11.8787,
-              lat = 51.3919,
-              zoom = 9
-            )
-        })
+          addMarkers(
+            lng = 11.8787,
+            lat = 51.3919,
+          )
+      })
 
-        # Calls update inputmap (leafletProxy fn) with the given coordinates (lng/lat or DEIMS.id)----
-        observeEvent(
-          coordinates(),
-          ignoreInit = TRUE,
-          ignoreNULL = TRUE,
-          {
-            grassland_update_map(
-              ns("leaflet_output"),
-              coordinates()
-            )
-          }
-        )
+      # Calls update inputmap (leafletProxy fn) with the given coordinates (lng/lat or DEIMS.id)----
+      observeEvent(
+        coordinates(),
+        ignoreInit = TRUE,
+        ignoreNULL = TRUE,
+        {
+          grassland_update_map(
+            ns("leaflet_output"),
+            coordinates()
+          )
+        }
+      )
 
-        w$hide()
-      }
-    )
+      w$hide()
+    })
   })
 }
